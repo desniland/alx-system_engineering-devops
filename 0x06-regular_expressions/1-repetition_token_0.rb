84ccfcf8c970a6bc403e100fb3d08cca502d0dec
desniland <albertdesmond26@gmail.com>
2024-02-27 19:41:18 +0300

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,23 +1,2 @@
 #!/usr/bin/env ruby
-require 'oniguruma'
-
-def find_matches(text, regex)
-  engine = Oniguruma::Engine.new(regex, options: Oniguruma::OPTION_MULTILINE)
-  matches = []
-  while match = engine.match(text)
-    matches << match.to_s
-    text = text[match.end..text.length]
-  end
-  if matches.empty?
-    puts "No matches found."
-  else
-    puts "Matches found:"
-    puts matches.join(" ")
-  end
-end
-
-regex = ARGV[0]
-
-text = "hbn hbtn hbttn hbtttn hbttttn hbtttttn hbttttttn"
-
-find_matches(text, regex)
+puts ARGV[0].scan(/hbt{2,5}n/).join
