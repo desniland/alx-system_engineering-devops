e852e99d0d7da382a3f221923fd3b7b36ec0c510
desniland <albertdesmond26@gmail.com>
2024-02-27 14:21:13 +0300

    Repetition Tokens

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/hbt{2,5}n/).join
+#def find_matches(text, regex)
+#  matches = text.scan(regex)
+#  if matches.empty?
+#    puts "No matches found."
+#  else
+#    puts "Matches found:"
+#    puts matches.join(" ")
+#  end
+#end
+#
+#
+#regex = ARGV[0]
+#
+#
+#text = "hbn hbtn hbttn hbtttn hbttttn hbtttttn hbttttttn"
+#
+#
+#find_matches(text, regex)
